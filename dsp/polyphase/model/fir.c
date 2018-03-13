15147ff33f0197bd7a2011e22650631453ea78e1
ban09 <cvazquez.alocen@gmail.com>
2018-03-13 21:30:09 +0100

    Refactor, tested 10x10 case

--- dsp/polyphase/model/fir.c
+++ dsp/polyphase/model/fir.c
@@ -5,10 +5,19 @@
 #define COEFF_SCALING (14) 
 
 /* NOTE it may not work if NCOEFFS/DECIMATING_FACTOR is not an integer */
-#define DECIMATING_FACTOR (3)
-#define NCOEFFS (9)
+#define DECIMATING_FACTOR (10)
+#define NCOEFFS (100)
 
-int COEFFS[NCOEFFS] =  {-95,0,1286,4121,5759,4121,1286,0,-95};
+//int COEFFS[NCOEFFS] =  {-95,0,1286,4121,5759,4121,1286,0,-95};
+int COEFFS[NCOEFFS] = {1, 3, 6, 8, 10, 11, 12, 10, 7, 3, -3, 
+    -11, -20, -28, -36, -40, -41, -36, -26, -10, 11, 35, 61,
+    85, 104, 114, 113, 98, 69, 26, -28, -91, -156, -216, -263,
+    -290, -288, -253, -181, -69, 78, 259, 465, 685, 908, 1120,
+    1309, 1462, 1571, 1627, 1627, 1571, 1462, 1309, 1120, 908,
+    685, 465, 259, 78, -69, -181, -253, -288, -290, -263, -216,
+    -156, -91, -28, 26, 69, 98, 113, 114, 104, 85, 61, 35, 11,
+    -10, -26, -36, -41, -40, -36, -28, -20, -11, -3, 3, 7, 10, 
+    12, 11, 10, 8, 6, 3, 1};
 
 int8_t buf;
 
@@ -23,7 +32,7 @@ int main(void){
         memmove(samples+1,samples,NCOEFFS-1);
         samples[0] = buf;
         acc = 0;
-        if ( ((i+1)%3)==0 ){
+        if ( ((i+1)%DECIMATING_FACTOR)==0 ){
             acc=0;
             for(int j = 0; j<NCOEFFS; j++){
                 acc+=samples[j]*COEFFS[j];
--- dsp/polyphase/model/fir_poly.c
+++ dsp/polyphase/model/fir_poly.c
@@ -3,38 +3,23 @@
 #include <string.h>
 #include <assert.h>
 
-#define STAGES (3)
-#define TAPS (3)
+#define STAGES (10)
+#define TAPS (10)
 
-//#define DUMP
-
-//int coeff_tmp[10][10] = { {1, -3, 11, -28, 78, 1627, -69, 26, -10, 3},
-//    {3, -11, 35, -91, 259, 1571, -181, 69, -26, 7},
-//    {6, -20, 61, -156, 465, 1462, -253, 98, -36, 10},
-//    {8, -28, 85, -216, 685, 1309, -288, 113, -41, 12},
-//    {10, -36, 104, -263, 908, 1120, -290, 114, -40, 11},
-//    {11, -40, 114, -290, 1120, 908, -263, 104, -36, 10},
-//    {12, -41, 113, -288, 1309, 685, -216, 85, -28, 8},
-//    {10, -36, 98, -253, 1462, 465, -156, 61, -20, 6},
-//    {7, -26, 69, -181, 1571, 259, -91, 35, -11, 3},
-//    {3, -10, 26, -69, 1627, 78, -28, 11, -3, 1}
-//};
-
-//int coeff_tmp[3][3] = { {-95,  4121, 1286},
-//                         {0,   5759,  0},
-//                        {1286, 4121, -95},
-//};
-//int coeff_tmp[3][3] = { {1286,  4121,  0},
-//                        {0,     4121,  1286},
-//                        {-95,   5759,  -95},
-//};
-int coeff_tmp[3][3] = { {1286,  4121,  -95},
-                        {0,     5759,  0},
-                        {-95,   4121,  1286},
-};
+/* TODO: Check coefficient distribution in the non-symmetric case */
+int coeff_tmp[STAGES][TAPS] = {
+    {3, -10, 26, -69, 1627, 78, -28, 11, -3, 1},
+ {7, -26, 69, -181, 1571, 259, -91, 35, -11, 3},
+ {10, -36, 98, -253, 1462, 465, -156, 61, -20, 6},
+ {12, -41, 113, -288, 1309, 685, -216, 85, -28, 8},
+ {11, -40, 114, -290, 1120, 908, -263, 104, -36, 10},
+ {10, -36, 104, -263, 908, 1120, -290, 114, -40, 11},
+ {8, -28, 85, -216, 685, 1309, -288, 113, -41, 12},
+ {6, -20, 61, -156, 465, 1462, -253, 98, -36, 10},
+ {3, -11, 35, -91, 259, 1571, -181, 69, -26, 7},
+ {1, -3, 11, -28, 78, 1627, -69, 26, -10, 3}};
 
 int main(){
-    int cnt = 0;
     int8_t din = 0;
     int8_t res = 0;
     int coeff[TAPS];
@@ -51,7 +36,6 @@ int main(){
 
     int add_tmp[STAGES];
     int add[STAGES]; 
-    int firstsample=0;
 
     memset(mul_in,0,STAGES*sizeof(int));
     memset(mul_in_tmp,0,STAGES*sizeof(int));
@@ -77,11 +61,10 @@ int main(){
                 }
             }
             for(int i = 0; i < STAGES; i++){
-                // Probably the efficient way to address the adressing problem in 
-                // hardware would be to rearrange the coefficients to account for 
-                // the pipeline.
-                //mul_tmp[i] = mul_in[i]*coeff_tmp[(idx-i)<0?(idx-i+3):(idx-i)][i];
-                mul_tmp[i] = mul_in[i]*coeff_tmp[((((idx-4*i)%TAPS)+TAPS)%TAPS)][i];
+                /* Probably the efficient way to address the adressing problem in 
+                 * hardware is to rearrange the coefficients to account for 
+                 * the pipeline.*/
+                mul_tmp[i] = mul_in[i]*coeff_tmp[((((idx-(STAGES+1)*i)%TAPS)+TAPS)%TAPS)][i];
                 if (i == 0){
                     add_tmp[i] = mul[i]; 
                 }
@@ -99,7 +82,6 @@ int main(){
             }
 
             // Update 
-
             for(int i = 0; i < STAGES-1; i++){
                 int fifo_tmp[STAGES+1];
                 memcpy(fifo_tmp,fifo[i],(STAGES+1)*sizeof(int));
@@ -112,18 +94,15 @@ int main(){
             }
             memcpy(mul_in, mul_in_tmp, STAGES*sizeof(int));
 
-            idx = (++idx)%TAPS;
+            if(++idx > TAPS-1){
+                idx = 0;
+            }
             memcpy(coeff,coeff_tmp[idx],STAGES*sizeof(int));
             memcpy(mul,mul_tmp,STAGES*sizeof(int));
             memcpy(add,add_tmp,STAGES*sizeof(int));
             out_acc = out_acc_tmp;
 
-          //  if(idx==2 && !firstsample){
-          //      res = (int8_t)(out_acc >> 14); 
-          //      fwrite(&res,1,1,stdout);
-          //  }
             clk++;
-
         }
     return 0;
 }
