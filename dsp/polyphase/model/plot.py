52bdcb58785e36321dbd508aca7d98321a2f5a41
ban09 <cvazquez.alocen@gmail.com>
2018-03-17 23:24:21 +0100

    Now VHDL simulation output is dumped to a file and plotted together with the C model

--- dsp/polyphase/model/plot.py
+++ dsp/polyphase/model/plot.py
@@ -4,11 +4,15 @@ from scipy import signal
 import io
 import utils.utils as utils
 
-x1 = np.fromfile("out_poly","int8",90000*10);
-x2 = np.fromfile("out_fir","int8",90000*10);
+length = int((90000/3)*9)
+x1 = np.fromfile("out_poly","int8",length);
+x2 = np.fromfile("out_fir","int8",length);
+x3 = np.fromfile("out_fpga","int8",length);
 
 w,fft1=utils.osfft(x1,90e6/10)
 w,fft2=utils.osfft(x2,90e6/10)
+w,fft3=utils.osfft(x3,90e6/10)
 plt.plot(w,fft1,'ro')
 plt.plot(w,fft2,'bx')
+plt.plot(w,fft3,'gx')
 plt.show()
--- dsp/polyphase/test/fir_tb.vhd
+++ dsp/polyphase/test/fir_tb.vhd
@@ -21,6 +21,7 @@ architecture test of fir_tb is
     signal dout_v : std_logic;
 
     signal start_sim : std_logic := '0';
+    signal stop_sim : std_logic := '0';
 
 
 begin
@@ -51,9 +52,28 @@ begin
         variable buff : std_logic_vector(DATA_WIDTH-1 downto 0);
     begin
         if start_sim = '1' then
-            read_word(fin,1,buff);
-            din <= buff;
-            din_v <= '1';
+            if not endfile(fin) then
+                read_word(fin,1,buff);
+                din <= buff;
+                din_v <= '1';
+            else
+                stop_sim <= '1';
+            end if;
+        end if;
+        wait until clk = '1';
+    end process;
+
+    -- Consumer
+    process is
+        file fout : binary_file_t open write_mode is "../../../../../../model/out_fpga";
+    begin
+        if stop_sim = '0' then
+            if dout_v = '1' then
+                write_byte(fout,dout);
+            end if;
+        else
+            file_close(fout);
+            assert false report "End of file" severity failure;
         end if;
         wait until clk = '1';
     end process;
